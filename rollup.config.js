568132df494eb2b1f1e9ace8bdf7cc30ea859a31
Sebastian Siemssen <fubhy@fubhy.com>
2018-01-13 09:14:24 +0100

    Simplifying rollup config.

--- rollup.config.js
+++ rollup.config.js
@@ -2,46 +2,24 @@ import babel from 'rollup-plugin-babel';
 import resolve from 'rollup-plugin-node-resolve';
 import commonjs from 'rollup-plugin-commonjs';
 
-export const globals = {
-  react: 'react',
-  'react-dom': 'reactDom',
-  ramda: 'ramda',
-  immutable: 'immutable',
-  'prop-types': 'propTypes',
-  invariant: 'invariant',
-};
+export const externals = ['react-query-builder'];
 
-export default (name, pkg) => {
-  const output = (file, format) => ({
-    file: file,
-    format: format,
+export default (name, pkg) => [
+  {
     name: name,
+    input: 'src/index.js',
+    output: [
+      { file: pkg.main, format: 'cjs' },
+      { file: pkg.module, format: 'es' },
+    ],
     exports: 'named',
-    globals: globals,
-  });
-
-  return [
-    {
-      input: 'src/index.js',
-      output: output(pkg.browser, 'umd'),
-      external: Object.keys(globals),
-      plugins: [
-        babel({
-          exclude: 'node_modules/**',
-        }),
-        resolve(),
-        commonjs(),
-      ],
-    },
-    {
-      input: 'src/index.js',
-      output: [output(pkg.main, 'cjs'), output(pkg.module, 'es')],
-      external: Object.keys(globals),
-      plugins: [
-        babel({
-          exclude: ['node_modules/**'],
-        }),
-      ],
-    },
-  ];
-};
+    external: externals.concat(Object.keys(pkg.peerDependencies)),
+    plugins: [
+      babel({
+        exclude: ['node_modules/**'],
+      }),
+      resolve(),
+      commonjs(),
+    ],
+  },
+];
